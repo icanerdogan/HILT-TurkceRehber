711b4f766991f01fab693bd49caf6d2053e9099f
Ibrahim.Erdogan <ibrahim.erdogan@logo.com.tr>
2022-08-30 13:18:50 +0300

    hilt v2

--- app/src/main/java/com/example/hiltdemo/MainActivity.kt
+++ app/src/main/java/com/example/hiltdemo/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.hiltdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.hiltdemo.util.Car
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val TAG = "MainActivity"
+    }
+
+    @Inject
+    lateinit var car: Car
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name = car.getCareName()
+        val batterySize = car.getBatterySize()
+
+        Log.d(TAG, name)
+        Log.d(TAG, batterySize)
     }
 }
